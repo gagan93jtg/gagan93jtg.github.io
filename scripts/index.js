ea087679a63699787b47aa2ffa3963e932807455
Gagandeep Singh <gagandeep@babatrucks.com>
2023-05-13 11:44:53 +0530

    Add button to blog on portfolio

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  $("#view-timeline-btn").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#section2").offset().top
-      },
-      300);
-  });
-});
